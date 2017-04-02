2c99f782820aa0f27db670bd85fc8590b8ef4510
Will Price <will.price94@gmail.com>
2017-04-02 20:34:59 +0100

    Chapter 2: vec3 class

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "vec3.hpp"
 
 int main() {
   int nx = 200;
@@ -6,17 +7,15 @@ int main() {
   std::cout << "P3\n" << nx << " " << ny << "\n255\n";
   for (int j = ny-1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
-      float r = float(i) / float(nx);
-      float g = float(j) / float(ny);
-      float b = 0.2;
+      vec3 color(float(i) / float(nx), float(j) / float(ny), 0.2);
       // I believe 255.99 is used instead of 255 since we're using the `int` constructor on floats, so we'll truncate the results
       //     0 <= r <= 1
       // =>  0 <= 255.99 * r <= 255.99
       // =>  0 <= int(255.99 * r) <= 255
       // It's a nasty way of rounding really, biasing towards rounding down.
-      int ir = int(255.99*r);
-      int ig = int(255.99*g);
-      int ib = int(255.99*b);
+      int ir = int(255.99*color.r());
+      int ig = int(255.99*color.g());
+      int ib = int(255.99*color.b());
       std::cout << ir << " " << ig << " " << ib << "\n";
     }
   }
